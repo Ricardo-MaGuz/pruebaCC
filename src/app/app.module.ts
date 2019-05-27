fb31ef152890eae5ff3615570f8b0f458fe79498
Ricardo-MaGuz <ricardo@wides.mx>
2019-05-26 22:00:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { HistoryComponent } from './history/history.component';
+import { JobComponent } from './job/job.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    MainNavComponent,
+    CompaniesComponent,
+    HistoryComponent,
+    JobComponent
   ],
   imports: [
     BrowserModule
